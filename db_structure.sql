a558653408bd6aa32af468da5caed93b0e2d34d6
cluosh <cluosh@shadow-code.org>
2014-04-09 13:24:47 +0200

    Updating db_structure.sql.

--- db_structure.sql
+++ db_structure.sql
@@ -3,6 +3,30 @@ SET foreign_key_checks = 0;
 SET time_zone = '+02:00';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `lwCMS_mod_inlineforms`;
+CREATE TABLE `lwCMS_mod_inlineforms` (
+  `form` varchar(30) NOT NULL,
+  `fields` text NOT NULL,
+  `checks` text NOT NULL,
+  PRIMARY KEY (`form`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `lwCMS_mod_inlineforms_data`;
+CREATE TABLE `lwCMS_mod_inlineforms_data` (
+  `form` varchar(30) NOT NULL,
+  `data` text NOT NULL,
+  `timestamp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `lwCMS_mod_inlineforms_info`;
+CREATE TABLE `lwCMS_mod_inlineforms_info` (
+  `form` varchar(30) NOT NULL,
+  `info` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 DROP TABLE IF EXISTS `lwCMS_pages`;
 CREATE TABLE `lwCMS_pages` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -13,8 +37,6 @@ CREATE TABLE `lwCMS_pages` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `lwCMS_pages` (`ID`, `pageID_string`, `page_title`, `theme_name`, `template_name`) VALUES
-(1,	'home',	'lwCMS',	'default',	'default');
 
 DROP TABLE IF EXISTS `lwCMS_pages_content`;
 CREATE TABLE `lwCMS_pages_content` (
@@ -33,3 +55,6 @@ CREATE TABLE `lwCMS_users` (
   `level` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `lwCMS_users` (`ID`, `name`, `passhash`, `level`) VALUES
+(1,	'admin',	'dd94709528bb1c83d08f3088d4043f4742891f4f',	0);
